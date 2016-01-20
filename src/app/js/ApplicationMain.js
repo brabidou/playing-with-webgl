ff4ba9a7e0a1cdb7bb80af7a768e8a7916cbe4be
Ben Rabidou <ben.rabidou@gmail.com>
2016-01-20 13:44:58 -0500

    fix bug

--- src/app/js/ApplicationMain.js
+++ src/app/js/ApplicationMain.js
@@ -36,13 +36,7 @@ ApplicationMain.prototype.init = function(  ) {
     var loading_screen = pleaseWait({
         logo: "img/synapse.png",
         backgroundColor: '#f46d3b',
-        loadingHtml: "<div class='sk-spinner sk-spinner-wave'>
-                        <div class='sk-rect1'></div>
-                        <div class='sk-rect2'></div>
-                        <div class='sk-rect3'></div>
-                        <div class='sk-rect4'></div>
-                        <div class='sk-rect5'></div>
-                    </div>"
+        loadingHtml: "<div class='sk-spinner sk-spinner-wave'><div class='sk-rect1'></div><div class='sk-rect2'></div><div class='sk-rect3'></div><div class='sk-rect4'></div><div class='sk-rect5'></div></div>"
     });
 
     gameService.GetGameForUser( game_id, player_id )
