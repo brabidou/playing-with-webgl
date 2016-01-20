8ba7e6d818fa87d0f8c11a3fea264e3bea9bca7a
Ben Rabidou <ben.rabidou@gmail.com>
2016-01-20 13:25:06 -0500

    more claneup

--- src/app/js/ApplicationMain.js
+++ src/app/js/ApplicationMain.js
@@ -22,7 +22,6 @@ ApplicationMain.prototype.init = function(  ) {
 
     var sceneManager = new SceneManager( true );
     var gameService = new GameService();
-    //var gameManager = new GameManager(sceneManager, gameService);
     var gameManager = new GameManager();
 
 
@@ -37,7 +36,13 @@ ApplicationMain.prototype.init = function(  ) {
     var loading_screen = pleaseWait({
         logo: "img/synapse.png",
         backgroundColor: '#f46d3b',
-        loadingHtml: "<div class='sk-spinner sk-spinner-wave'><div class='sk-rect1'></div><div class='sk-rect2'></div><div class='sk-rect3'></div><div class='sk-rect4'></div><div class='sk-rect5'></div></div>"
+        loadingHtml: "<div class='sk-spinner sk-spinner-wave'>
+                        <div class='sk-rect1'></div>
+                        <div class='sk-rect2'></div>
+                        <div class='sk-rect3'></div>
+                        <div class='sk-rect4'></div>
+                        <div class='sk-rect5'></div>
+                    </div>"
     });
 
     gameService.GetGameForUser( game_id, player_id )
